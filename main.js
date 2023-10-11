978ffb8bc9a2cb00afe8b5c4dec8ac20e9333efe
ANCAKE <113901391+Ancake@users.noreply.github.com>
2023-10-11 12:38:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector (idElementoAudio).play();
-}
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
 
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+//o que é uma constante...
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
-for ()
+
+//document.querySelector('.teclaPom').onecçick = tocaSomPom;
+
+//document.querySelector('.tecla_pom');
+//document.querySelector('#som_tecla_pom').play();
